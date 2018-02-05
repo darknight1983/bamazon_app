9361c6f21fd3c111716172bfc1b7dabf256a76a9
Jacolby Green <darknightnov@gmail.com>
2018-02-05 13:45:16 -0600

    Added makeOrder feature that updates the database on successful orders

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -33,19 +33,44 @@ const Bamazon = {
       this.promptCustomer();
     });
   },
-  makeOrder: function(id, quanity) {
+  makeOrder: function(id, quanity, inStock, price) {
+    // Use arguements to build database query.
+    let query = `UPDATE products SET stock_quanity = ${inStock - quanity} WHERE item_id = ${id}`;
 
+    // Make the order and update the database.
+    connection.query(query, (err, results, fields) => {
+      if (err) {
+        throw err;
+      }
+
+      let orderAmount = (quanity * price);
+      // Show the customer the total amount that the would have to pay for the order.
+      console.log(`Your total is $${orderAmount}`);
+    })
 
   },
   checkInventory(id, quanity) {
     // Method responsible for making sure there is enough units to fill the customers order.
-    connection.query('SELECT stock_quanity FROM products WHERE stock_quanity > ?')
+    connection.query(`SELECT stock_quanity, price FROM products WHERE item_id = ${id}`, (err, results, fields) => {
+      if (err) {
+        throw err;
+      }
+      // Create a reference to the number of items and the price of the item.
+      let inStock = results[0].stock_quanity;
+      let price = results[0].price;
+
+      // Compare the quantity from the db to the amount the customer would like to purchase.
+      (results[0].stock_quanity > quanity) ?
+      this.makeOrder(id, quanity, inStock, price) :
+       console.log(`We currently dont have that amount in stock.`);
+       connection.end();
+    })
   },
   promptCustomer: function() {
     inquirer.prompt({
       type: 'input',
       name: 'id',
-      message: 'What is the ID of the project you would like to buy?'
+      message: 'What is the item_id of the product you would like to buy?'
     }).then((answer) => {
       // Store the ID in while prompting the customer for quanity.
       this.item_id = answer.id;
